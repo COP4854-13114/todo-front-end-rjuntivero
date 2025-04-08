8d190fe6167eaef1b337082be061e06d3c509330
rjuntivero <jirokuntivero@gmail.com>
2025-04-08 17:32:36 -0400

    chore: separated server and client

--- src/models/error/NoAuthMessage.model.ts
+++
@@ -1,7 +0,0 @@
-export class NoAuthMessage extends Error {
-  statusCode: number;
-  constructor(message: string, statusCode: number = 401) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
